83ff3f9be2f3f58fe691cc1d50a8cf2876881ef9
harell <harel@loostiger.com>
2019-05-19 17:57:38 +1200

    install devtools

--- .github-ci/test-suite.R
+++ .github-ci/test-suite.R
@@ -5,6 +5,20 @@ Sys.setenv(NOT_CRAN = "true")
 job_name <- tolower(Sys.getenv("TRAVIS_JOB_NAME"))
 message(rep("#",40), "\n", "## Test Suite: ", job_name, "\n", rep("#",40))
 
+###########################################
+## Install the Package Under Development ##
+###########################################
+devtools::install_local(
+    path = ".",
+    dependencies = TRUE,
+    upgrade = FALSE,
+    force = TRUE,
+    build = FALSE,
+    build_opts = "--no-multiarch --with-keep.source --no-build-vignettes",
+    Ncpus = parallel::detectCores(),
+    repos = "https://cloud.r-project.org"
+)
+
 #######################
 ## Get Project Paths ##
 #######################
