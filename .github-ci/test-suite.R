ef3585b09f8945d6e3e4f699522ee41a00ae9b84
harell <harel@loostiger.com>
2019-05-19 17:35:47 +1200

    travis runs unit tests

---
+++ .github-ci/test-suite.R
@@ -0,0 +1,28 @@
+###########
+## Setup ##
+###########
+Sys.setenv(NOT_CRAN = "true")
+job_name <- tolower(Sys.getenv("TRAVIS_JOB_NAME"))
+message(rep("#",40), "\n", "## Test Suite: ", job_name, "\n", rep("#",40))
+
+#######################
+## Get Project Paths ##
+#######################
+## Remove the word "test" from the package path
+path_project <- getwd()
+while (length(grep("test", path_project))>0) path_project <- dirname(path_project)
+## Get package name
+package_name <- basename(path_project)
+## Create other package paths
+path_functions <- file.path(path_project, "R")
+path_tests <- file.path(path_project, "tests", job_name)
+path_temp <- file.path(path_project, "temp")
+
+###############
+## Run Tests ##
+###############
+library(package_name, character.only = TRUE)
+testthat::test_dir(path_tests,
+                   show_report = TRUE,
+                   stop_on_failure = TRUE,
+                   package = package_name)
