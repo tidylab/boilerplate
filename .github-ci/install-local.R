e181e87044a090f9684010670a13da198b700a13
harell <harel@loostiger.com>
2019-05-24 06:53:20 +1200

    install without loading package

--- .github-ci/install-local.R
+++ .github-ci/install-local.R
@@ -1,9 +1,9 @@
 # Helper functions -------------------------------------------------------------
 .install_development_packages <- function(){
-    .library("tidyverse")
-    .library("devtools")
-    .library("testthat")
-    .library("covr")
+    .install.packages("tidyverse")
+    .install.packages("devtools")
+    .install.packages("testthat")
+    .install.packages("covr")
 }
 
 .install_local_package <- function(){
@@ -21,14 +21,17 @@
 }
 
 .library <- function(package){
+    .install.packages(package)
+    library(package, character.only = TRUE)
+}
+
+.install.packages <- function(package){
     if(!require(package, character.only = TRUE))
         utils::install.packages(package,
                                 repos = "https://cloud.r-project.org",
                                 dependencies = TRUE,
                                 Ncpus = parallel::detectCores()
         )
-
-    library(package, character.only = TRUE)
 }
 
 # Install local package --------------------------------------------------------
