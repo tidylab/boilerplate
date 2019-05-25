449a62d09256382c7fb89a13bded9a7018338e04
harell <harel@loostiger.com>
2019-05-25 14:53:45 +1200

    reinstall package during local-install

--- .github-ci/install-local.R
+++ .github-ci/install-local.R
@@ -1,4 +1,9 @@
 # Helper functions -------------------------------------------------------------
+.uninstall_local_package <- function(){
+    try(remove.packages(.get_package_name()), silent = TRUE)
+    return(invisible())
+}
+
 .install_development_packages <- function(){
     .install.packages("tidyverse")
     .install.packages("devtools")
@@ -34,6 +39,17 @@
         )
 }
 
+.get_package_name <- function(){
+    return(gsub(".Rcheck$", "", basename(.getwd())))
+}
+
+.getwd <- function(){
+    path_project <- getwd()
+    while (length(grep("test", path_project))>0) path_project <- dirname(path_project)
+    return(path_project)
+}
+
 # Install local package --------------------------------------------------------
+.uninstall_local_package()
 .install_development_packages()
 .install_local_package()
