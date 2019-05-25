fdeba19da727285fe0857be346af3a1b3277e4e4
harell <harel@loostiger.com>
2019-05-25 14:17:25 +1200

    skip loading package in CI test suite

--- .github-ci/test-suite.R
+++ .github-ci/test-suite.R
@@ -9,12 +9,9 @@
 Sys.setenv(NOT_CRAN = "true")
 job_name <- tolower(Sys.getenv("TRAVIS_JOB_NAME"))
 message(rep("#",40), "\n", "## Test Suite: ", job_name, "\n", rep("#",40))
-path_project <- .getwd()
-package_name <- basename(path_project)
-path_tests <- file.path(path_project, "tests", job_name)
+path_tests <- file.path(.getwd(), "tests", job_name)
 
 # Run tests --------------------------------------------------------------------
-library(package_name, character.only = TRUE)
 testthat::test_dir(path_tests,
                    show_report = TRUE,
                    stop_on_failure = TRUE,
