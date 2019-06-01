78040ba49c43aaa8f4dacfa3dbd77f21837f5f00
harell <harel@loostiger.com>
2019-06-01 13:28:44 +1200

    fix bug

--- .github-ci/reports.R
+++ .github-ci/reports.R
@@ -1,9 +1,15 @@
+# Helper Functions -------------------------------------------------------------
+.codecov <- function(){
+    Sys.setenv(TESTTHAT = "true")
+    covr::codecov()
+    Sys.setenv(TESTTHAT = "")
+}
+
 # Setup ------------------------------------------------------------------------
 job_name <- tolower(Sys.getenv("TRAVIS_JOB_NAME"))
 message(rep("#",40), "\n", "## ", job_name, "\n", rep("#",40))
 
-
 # Run Reports ------------------------------------------------------------------
 switch (job_name,
-    "coverage-report" = covr::codecov()
+        "coverage-report" = .codecov()
 )
