41d9e1ade05dcc8c995a48ae198aa85b329fb64c
harell <harel@loostiger.com>
2019-07-07 15:42:41 +1200

    step_build_and_check

--- .tic/build-and-check.R
+++
@@ -1,3 +0,0 @@
-# Programming Logic -------------------------------------------------------
-system("R CMD build . --no-build-vignettes --no-manual")
-system("R CMD check $(ls -1t *.tar.gz | head -n 1) --no-manual")
--- tic.R
+++ tic.R
@@ -1,4 +1,5 @@
 source(file.path(getwd(), ".tic", "helpers-tic.R"))
+source(file.path(getwd(), ".tic", "BuildAndCheck.R"))
 source(file.path(getwd(), ".tic", "TestSuite.R"))
 
 # Stage: Before Script ---------------------------------------------------------
@@ -8,11 +9,16 @@ get_stage("before_script") %>%
     add_step(step_run_code(devtools::document()))
 
 # Stage: Script ----------------------------------------------------------------
-if(ci_get_job_name() %in% c("testthat", "component-tests", "integration-tests", "coverage-tests"))
+if(ci_get_job_name() %in% c("build")){
     get_stage("script") %>%
-    add_step(step_install_cran("desc")) %>%
-    add_step(step_install_cran("testthat")) %>%
-    add_step(step_run_test_suite(job_name = ci_get_job_name()))
+        add_step(step_build_and_check())
 
+} else if(ci_get_job_name() %in% c("testthat", "component-tests", "integration-tests", "coverage-tests")){
+    get_stage("script") %>%
+        add_step(step_install_cran("desc")) %>%
+        add_step(step_install_cran("testthat")) %>%
+        add_step(step_run_test_suite(job_name = ci_get_job_name()))
+
+}
 # do_package_checks()
 
