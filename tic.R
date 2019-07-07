c2be688f1c0da4cf364e0d6d599d37658a1ff932
harell <harel@loostiger.com>
2019-07-07 12:38:44 +1200

    install local package

--- tic.R
+++ tic.R
@@ -1,3 +1,11 @@
+source(file.path(getwd(), ".tic", "helper-functions.R"))
+
+# Install local package --------------------------------------------------------
+.uninstall_local_package()
+.install_development_packages()
+.install_local_package()
+
+
 do_package_checks()
 
 # if (ci_on_travis()) {
