9b6245a7caae9a44b8aed520869c7755e9ec3126
harell <harel@loostiger.com>
2019-07-07 15:07:31 +1200

    source at the outset

--- tic.R
+++ tic.R
@@ -1,3 +1,5 @@
+source(file.path(getwd(), ".tic", "TestSuite.R"))
+
 # Stage: Before Script ---------------------------------------------------------
 get_stage("before_script") %>%
     add_step(step_install_cran("devtools")) %>%
@@ -8,7 +10,6 @@ get_stage("before_script") %>%
 get_stage("script") %>%
     add_step(step_install_cran("desc")) %>%
     add_step(step_install_cran("testthat")) %>%
-    add_step(step_run_code(source(file.path(getwd(), ".tic", "TestSuite.R")))) %>%
     add_step(step_run_test_suite())
 
 # do_package_checks()
