56ba852f50eebd8aa4447eaf299a908411cef3bc
harell <harel@loostiger.com>
2019-07-07 14:47:07 +1200

    TestSuite R6 object

---
+++ .tic/TestSuite.R
@@ -0,0 +1,22 @@
+TestSuite <- R6Class(
+    "TestSuite", inherit = TicStep,
+
+    public = list(
+        run = function() {
+            ci_get_job_name <- function() tolower(paste0(Sys.getenv("TRAVIS_JOB_NAME"), Sys.getenv("APPVEYOR_JOB_NAME")))
+
+            package_name <- desc::description$new()$get_field("Package")
+
+            library(package_name, character.only = TRUE)
+
+            testthat::test_dir(file.path(getwd(), "tests", ci_get_job_name()),
+                               show_report = TRUE,
+                               stop_on_failure = TRUE,
+                               package = package_name)
+        }
+    )
+)
+
+step_run_test_suite <- function(){
+    TestSuite$new()
+}
--- tic.R
+++ tic.R
@@ -1,3 +1,5 @@
+source(file.path(getwd(), ".tic", "TestSuite.R"))
+
 # Stage: Before Script ---------------------------------------------------------
 get_stage("before_script") %>%
     add_step(step_install_cran("devtools")) %>%
@@ -8,16 +10,7 @@ get_stage("before_script") %>%
 get_stage("script") %>%
     add_step(step_install_cran("desc")) %>%
     add_step(step_install_cran("testthat")) %>%
-    add_step(step_run_code(library(desc::description$new()$get_field("Package"), character.only = TRUE))) %>%
-    add_step(step_run_code(source(file.path(getwd(), ".tic", "helpers-tic.R")))) %>%
-    add_step(step_run_code(
-        testthat::test_dir(file.path(getwd(), "tests", ci_get_job_name()),
-                           show_report = TRUE,
-                           stop_on_failure = TRUE,
-                           package = desc::description$new()$get_field("Package"))
-    ))
-
-
+    add_step(step_run_test_suite())
 
 # do_package_checks()
 
