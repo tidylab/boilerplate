5b940dd85b4c274dadd04bb8f94e27159e5331ee
harell <harel@loostiger.com>
2019-07-07 14:55:34 +1200

    Before Script

--- tic.R
+++ tic.R
@@ -2,13 +2,13 @@
 get_stage("before_script") %>%
     add_step(step_install_cran("devtools")) %>%
     add_step(step_install_cran("roxygen2")) %>%
-    add_step(step_run_code(devtools::document()))
+    add_step(step_run_code(devtools::document())) %>%
+    add_step(step_run_code(source(file.path(getwd(), ".tic", "TestSuite.R"))))
 
 # Stage: Script ----------------------------------------------------------------
 get_stage("script") %>%
     add_step(step_install_cran("desc")) %>%
     add_step(step_install_cran("testthat")) %>%
-    add_step(step_run_code(source(file.path(getwd(), ".tic", "TestSuite.R")))) %>%
     add_step(step_run_test_suite())
 
 # do_package_checks()
