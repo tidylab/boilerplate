1ce362b5acc327b24071ff35fbf170b89b5842ee
harell <harel@loostiger.com>
2019-12-30 10:12:52 +1300

    updated tic

--- tic.R
+++ tic.R
@@ -1,15 +1,7 @@
-library(tic)
-invisible(sapply(list.files("./.tic", full.names = TRUE), source))
+# installs dependencies, runs R CMD check, runs covr::codecov()
+tic::do_package_checks()
 
-# Stage: Before Script ---------------------------------------------------------
-get_stage("before_script") %>%
-    add_step(step_run_code(devtools::document()))
-
-# Stage: Script ----------------------------------------------------------------
-get_stage("script") %>%
-    add_step(step_run_test_suite(job_name = ci_get_job_name())) %>%
-    add_step(step_render_report(job_name = ci_get_job_name()))
-
-# Stage: After Failure ----------------------------------------------------------
-get_stage("after_failure") %>%
-    add_step(step_run_code(show_error_log()))
+# if (ci_on_travis()) {
+#   # creates pkgdown site and pushes to gh-pages branch
+#   do_pkgdown()
+# }
