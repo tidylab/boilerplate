b7b07a467341e886b0fb1400854987e5e7eb07c0
harell <harel@loostiger.com>
2019-12-30 13:06:12 +1300

    add steps

--- tic.R
+++ tic.R
@@ -4,13 +4,12 @@ invisible(sapply(list.files("./.app/tic", full.names = TRUE), source))
 # Stage: Install ----------------------------------------------------------
 get_stage("install") %>%
     add_step(step_run_code(devtools::document())) %>%
-    # Configures R session to install packages as they existed on CRAN at time
-    # of package Date field in DESCRIPTION file
     add_step(step_run_code(set_repos_to_MRAN())) %>%
-    add_step(step_run_code(remotes::install_local(dependencies = TRUE)))
+    add_step(step_install_deps())
 
 # Stage: Script ----------------------------------------------------------------
 get_stage("script") %>%
+    add_step(step_build_and_check(job_name = ci_get_job_name())) %>%
     add_step(step_run_test_suite(job_name = ci_get_job_name())) %>%
     add_step(step_render_report(job_name = ci_get_job_name()))
 
