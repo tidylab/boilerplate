13fa2a3ccf67a1944334ef81b7eb6343005a267b
harell <harel@loostiger.com>
2020-02-23 21:30:51 +1300

    3 stages

--- tic.R
+++ tic.R
@@ -7,6 +7,7 @@
 #' 5. OPTIONAL deploy
 #' 6. OPTIONAL after_deploy
 #'
+source("./AppData/travis/helpers.R")
 
 # Stage: Before Script ----------------------------------------------------
 get_stage("before_script") %>%
@@ -14,9 +15,15 @@ get_stage("before_script") %>%
     add_step(step_run_code(try(devtools::uninstall(), silent = TRUE)))
 
 # Stage: Script -----------------------------------------------------------
-get_stage("script") %>%
-    add_step(step_run_code(devtools::document(quiet = TRUE))) %>%
-    add_step(step_rcmdcheck(error_on = "error"))
+if(ci_get_job_name() == "build"){
+    get_stage("script") %>%
+        add_step(step_run_code(devtools::document(quiet = TRUE))) %>%
+        add_step(step_rcmdcheck(error_on = "error"))
+} else if (tic::ci_get_branch() == "test") {
+    get_stage("script") %>%
+        add_step(step_run_code(devtools::load_all(export_all = FALSE))) %>%
+        add_step(step_run_code(testthat::test_dir("./tests/testthat")))
+}
 
 # Stage: After Success ----------------------------------------------------
 get_stage("after_success")
@@ -30,7 +37,7 @@ get_stage("before_deploy")
 
 # Stage: Deploy -----------------------------------------------------------
 get_stage("deploy") %>%
-    add_step(tic::step_build_pkgdown())
+    add_step(step_build_pkgdown())
 
 # Stage: After Deploy -----------------------------------------------------
 # get_stage("after_deploy") %>%
