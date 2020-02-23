1d09390a3d909b63c78895a9d241b4d0d15eceea
harell <harel@loostiger.com>
2020-02-23 17:26:10 +1300

    updated travis

--- tic.R
+++ tic.R
@@ -1,25 +1,7 @@
-library(tic)
-if(!requireNamespace("desc")) remotes::install_version("desc", "1.2.0")
-invisible(sapply(list.files("./.app/tic", full.names = TRUE), source))
+# installs dependencies, runs R CMD check, runs covr::codecov()
+do_package_checks()
 
-# Stage : Before Install -------------------------------------------------------
-get_stage("before_install") %>%
-    add_step(step_run_code(print(Sys.getenv())))
-
-# Stage: Install ---------------------------------------------------------------
-get_stage("install") %>%
-    add_step(step_run_code(set_repos_to_MRAN())) %>%
-    add_step(step_install_cran("devtools", repos = get_MRAN_URL())) %>%
-    add_step(step_install_deps(repos = get_MRAN_URL()))
-
-# Stage: Script ----------------------------------------------------------------
-get_stage("script") %>%
-    add_step(step_run_code(set_repos_to_MRAN())) %>%
-    add_step(step_run_code(devtools::document())) %>%
-    add_step(step_build_and_check(job_name = ci_get_job_name())) %>%
-    add_step(step_run_test_suite(job_name = ci_get_job_name())) %>%
-    add_step(step_deploy(job_name = ci_get_job_name()))
-
-# Stage: After Failure ---------------------------------------------------------
-get_stage("after_failure") %>%
-    add_step(step_run_code(sessioninfo::session_info(include_base = FALSE)))
+if (ci_on_travis()) {
+  # creates pkgdown site and pushes to gh-pages branch
+  do_pkgdown()
+}
