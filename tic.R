2b018db9e3b0ffde725aaffdfc34f7788ecf59f7
harell <harel@loostiger.com>
2020-02-23 17:41:51 +1300

    one step only

--- tic.R
+++ tic.R
@@ -1,7 +1,51 @@
-# installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks()
+#' Job Lifecycle
+#'
+#' 1. before_script
+#' 2. script
+#' 3. after_success or after_failure
+#' 4. OPTIONAL before_deploy
+#' 5. OPTIONAL deploy
+#' 6. OPTIONAL after_deploy
+#'
 
-if (ci_on_travis()) {
-  # creates pkgdown site and pushes to gh-pages branch
-  do_pkgdown()
-}
+# Stage: Before Script ----------------------------------------------------
+get_stage("before_script") %>%
+    add_step(step_run_code(remotes::install_deps(dependencies = "Imports", build = FALSE, quiet = TRUE))) %>%
+    add_step(step_run_code(try(devtools::uninstall(), silent = TRUE)))
+
+# Stage: Script -----------------------------------------------------------
+get_stage("script") %>%
+    add_step(step_run_code(devtools::document(quiet = TRUE))) %>%
+    add_step(step_rcmdcheck(error_on = "error"))
+
+# Stage: After Success ----------------------------------------------------
+get_stage("after_success")
+
+# Stage: After Failure ----------------------------------------------------
+get_stage("after_failure") %>%
+    add_step(step_run_code(print(sessioninfo::session_info(include_base = FALSE))))
+
+# Stage: Before Deploy ----------------------------------------------------
+get_stage("before_deploy")
+
+# Stage: Deploy -----------------------------------------------------------
+get_stage("deploy")
+
+# Stage: After Deploy -----------------------------------------------------
+# get_stage("after_deploy") %>%
+#     add_step(step_install_cran("covr")) %>%
+#     add_step(step_run_code(covr::package_coverage(type = c("tests", "examples"), quiet = FALSE)))
+
+
+
+
+
+
+
+# # installs dependencies, runs R CMD check, runs covr::codecov()
+# do_package_checks()
+#
+# if (ci_on_travis()) {
+#   # creates pkgdown site and pushes to gh-pages branch
+#   do_pkgdown()
+# }
